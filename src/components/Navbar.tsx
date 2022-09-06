b52bb1bbec24435d5faf5f5f690c985562f1f0aa
er-ebrahimi <erfan.es1381@gmail.com>
2022-09-06 12:28:16 +0430

    log in button and shopping svg added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,24 @@
 import { Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 // import {Navbar as NavbarBs} from "react-bootstrap"
 export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="container nav--container">
-        <a className="logo--container">arsen</a>
+        <div className="shop">
+          <a className="logo--container">arsen</a>
+          <Nav.Link className="shop--link">
+            <FontAwesomeIcon
+              icon={faShoppingCart}
+              className="fa-cart-shopping"
+            />
+          </Nav.Link>
+          <button className="button login--button">log in</button>
+        </div>
+
         <nav className="nav">
           <ul className="nav--ul">
             <Nav.Link
